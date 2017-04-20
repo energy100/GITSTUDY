d1731a32c16e92e7ed2593a5dc3d777c72045e59
민동규 <energy100@korea.ac.kr>
2017-04-21 05:11:55 +0900

    Initial Commit

---
+++ PA#2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PA#2
+//
+//  Created by macbook air on 2017. 4. 21..
+//  Copyright © 2017년 macbook air. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
